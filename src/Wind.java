74796150833996cec48c2ee63f4db54f2b8f6b70
kaz56789 <819977@seq.org>
2026-05-19 20:35:45 -0700

    Update Wind.java

--- src/Wind.java
+++ src/Wind.java
@@ -1,31 +1,37 @@
-import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.Font;
 import java.util.Random;
 
 public class Wind {
-    private double speed; // e.g., 0.0 to 10.0
-    private double directionX; // -1.0 to 1.0 (Left or Right)
-    
-    private Random random;
+    private double force; 
+    private Random random = new Random();
 
     public Wind() {
-        random = new Random();
         randomize();
     }
 
     public void randomize() {
-        // Wind has been removed
-        speed = 0.0;
-        directionX = 0.0;
-    }
-    
-    public double getWindForce() {
-        return speed * directionX;
+        force = -3.0 + random.nextDouble() * 6.0;
     }
 
-    public void draw(Graphics2D g, int screenWidth, int screenHeight) {
-        // Wind UI is hidden
+    public double getForce() {
+        return force;
+    }
+
+    public void draw(Graphics2D g2d, int width, int height) {
+        g2d.setFont(new Font("SansSerif", Font.BOLD, 18));
+        g2d.setColor(Color.WHITE);
+        
+        String windText = "";
+        if (force < -0.5) {
+            windText = "Wind: <<< Left (" + String.format("%.1f", Math.abs(force)) + " m/s)";
+        } else if (force > 0.5) {
+            windText = "Wind: Right >>> (" + String.format("%.1f", force) + " m/s)";
+        } else {
+            windText = "Wind: Calm (0.0 m/s)";
+        }
+        
+        g2d.drawString(windText, width - 260, 40);
     }
 }
