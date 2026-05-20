2f0f3a1cff19728ee8820332d19ba8ffcb2c7833
kaz56789 <819977@seq.org>
2026-05-19 22:38:31 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Archery Simulation Mechanics");
-        GamePanel gamePanel = new GamePanel();
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.add(gamePanel);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-
-        // Ignite the calculation looping systems
-        gamePanel.startGame();
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("tuffgame");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setResizable(false);
+            
+            GamePanel gamePanel = new GamePanel();
+            frame.add(gamePanel);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            
+            gamePanel.startGame();
+        });
     }
 }
